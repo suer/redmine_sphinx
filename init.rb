039fb179fde72321cbfcbf0c998353f0315f4f45
suer <suetsugu.r@gmail.com>
2013-07-10 15:02:00 +0900

    Wrote: init.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_sphinx do
   name 'Redmine Sphinx plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'suer'
+  description 'This is a plugin for Redmine that support bulidig Sphinx documents'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/suer/redmine_sphinx'
+  author_url 'http://d.hatena.ne.jp/suer/'
 end
